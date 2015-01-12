0f5a2f1f4eb38c1b1e051621ccd90c6f28becf84
王雪梅 <wxm112@me.com>
2015-01-13 09:16:39 +1100

    first commit

---
+++ string.rb
@@ -0,0 +1,15 @@
+puts "What is your first name?"
+
+first_name = gets.chomp
+
+puts "What is your surname?"
+
+last_name = gets.chomp
+
+puts "What is your suburb?"
+
+suburb = gets.chomp
+
+puts "Your full name is #{first_name} #{last_name}"
+puts "You live in #{suburb}."
+ 
