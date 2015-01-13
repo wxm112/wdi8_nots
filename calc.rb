82701f19ec16a47c9192b150569cb210778a4d89
王雪梅 <wxm112@me.com>
2015-01-13 19:10:01 +1100

    calc.rb: reformat using BeautifyRuby

--- calc.rb
+++ calc.rb
@@ -2,89 +2,89 @@
 # require 'rainbow'
 
 def read_choice
-	puts "Welcome to May's Calculator!"
-	puts "Available functions: "
-	puts "1. Addition"
-	puts "2. Subtract"
-	puts "3. Multiply"
-	puts "4. Division"
-	puts "5. Exponents"
-	puts "6. Square roots"
+  puts "Welcome to May's Calculator!"
+  puts "Available functions: "
+  puts "1. Addition"
+  puts "2. Subtract"
+  puts "3. Multiply"
+  puts "4. Division"
+  puts "5. Exponents"
+  puts "6. Square roots"
 
-	puts "Q.Quit"
+  puts "Q.Quit"
 
-	print "Please enter your selections: "
+  print "Please enter your selections: "
 
-	gets.chomp.downcase
+  gets.chomp.downcase
 end
 
 def add(number1,number2)
-	number1 + number2
+  number1 + number2
 end
 
 def sub(number1,number2)
-	number1 - number2
+  number1 - number2
 end
 
 def multply(number1,number2)
-	number1 * number2
-end 
+  number1 * number2
+end
 
 def division(number1,number2)
-	number1 / number2
+  number1 / number2
 end
 
 def expo(number1,number2)
-	number1 ** number2
+  number1 ** number2
 end
 
 def square_roots(n)
-	Math.sqrt(n)
+  Math.sqrt(n)
 end
 
 menu_selection = read_choice
 
 if menu_selection != "q" && menu_selection != '6'
-	print "Enter the first number: "
-	n1 = gets.to_i
-	print "Enter the second number: "
-	n2 = gets.to_i
+  print "Enter the first number: "
+  n1 = gets.to_i
+  print "Enter the second number: "
+  n2 = gets.to_i
 elsif menu_selection == '6'
-	print "Enter your number: "
-	n = gets.to_i	
-end	
+  print "Enter your number: "
+  n = gets.to_i
+end
 
 while menu_selection != "q"
-	case menu_selection
-	when '1'
-		result = add(n1,n2)
-	when '2'
-		result = sub(n1,n2)
-	when '3'
-		result = multply(n1,n2)
-	when '4'
-		result = division(n1,n2)
-	when '5'
-		result = expo(n1,n2)
-	when '6'
-		result = square_roots(n)
-	else
-		result = nil
-		puts "Unknown function"
-	end
+  case menu_selection
+  when '1'
+    result = add(n1,n2)
+  when '2'
+    result = sub(n1,n2)
+  when '3'
+    result = multply(n1,n2)
+  when '4'
+    result = division(n1,n2)
+  when '5'
+    result = expo(n1,n2)
+  when '6'
+    result = square_roots(n)
+  else
+    result = nil
+    puts "Unknown function"
+  end
 
-	puts "The result is: #{result}" unless result.nil?
+  puts "The result is: #{result}" unless result.nil?
 
-	menu_selection = read_choice
-		if menu_selection != "q" && menu_selection != '6'
-			print "Enter the first number: "
-			n1 = gets.to_i
-			print "Enter the second number: "
-			n2 = gets.to_i
-		elsif menu_selection == '6'
-			print "Enter your number: "
-			n = gets.to_i	
-		end	
+  menu_selection = read_choice
+  if menu_selection != "q" && menu_selection != '6'
+    print "Enter the first number: "
+    n1 = gets.to_i
+    print "Enter the second number: "
+    n2 = gets.to_i
+  elsif menu_selection == '6'
+    print "Enter your number: "
+    n = gets.to_i
+  end
 end
 
 puts "Thank you for using Crappy Calc"
