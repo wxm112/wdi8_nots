2ed98d72d4fc4a56c353602d8a081445a0dde34f
王雪梅 <wxm112@me.com>
2015-01-13 19:06:03 +1100

    calc.rb: moved 'puts' outside of calculation functions

--- calc.rb
+++ calc.rb
@@ -19,33 +19,27 @@ def read_choice
 end
 
 def add(number1,number2)
-	result = number1 + number2
-	puts "The result is: #{result}"
+	number1 + number2
 end
 
 def sub(number1,number2)
-	result = number1 - number2
-	puts "The result is: #{result}"
+	number1 - number2
 end
 
 def multply(number1,number2)
-	result = number1 * number2
-	puts "The result is: #{result}"
+	number1 * number2
 end 
 
 def division(number1,number2)
-	result = number1 / number2
-	puts "The result is: #{result}"
+	number1 / number2
 end
 
 def expo(number1,number2)
-	result = number1 ** number2
-	puts "The result is: #{result}"
+	number1 ** number2
 end
 
 def square_roots(n)
-	result = Math.sqrt(n)
-	puts "The result is: #{result}"
+	Math.sqrt(n)
 end
 
 menu_selection = read_choice
@@ -63,20 +57,24 @@ end
 while menu_selection != "q"
 	case menu_selection
 	when '1'
-		add(n1,n2)
+		result = add(n1,n2)
 	when '2'
-		sub(n1,n2)
+		result = sub(n1,n2)
 	when '3'
-		multply(n1,n2)
+		result = multply(n1,n2)
 	when '4'
-		division(n1,n2)
+		result = division(n1,n2)
 	when '5'
-		expo(n1,n2)
+		result = expo(n1,n2)
 	when '6'
-		square_roots(n)
+		result = square_roots(n)
 	else
+		result = nil
 		puts "Unknown function"
 	end
+
+	puts "The result is: #{result}" unless result.nil?
+
 	menu_selection = read_choice
 		if menu_selection != "q" && menu_selection != '6'
 			print "Enter the first number: "
