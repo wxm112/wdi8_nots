1db22986cf4783c542a1c72b3caa6db4a06196c8
王雪梅 <wxm112@me.com>
2015-01-14 18:04:37 +1100

    from n to l

--- mta.rb
+++ mta.rb
@@ -13,7 +13,7 @@ def rute(start, ending)
   line_l = ['8th', '6th', 'union square', '3rd', '1st']
   line_6 = ['grand central', '33rd', '28th', '23rd', 'union square', 'astro place']
   if start[0] == ending[0]
-    if start[0].downcase == "n"
+    if start[0].downcase == "n" # n to n
       s_index = line_n.index(start[1])
       e_index = line_n.index(ending[1])
       if s_index < e_index
@@ -26,7 +26,7 @@ def rute(start, ending)
         puts "There are #{numbers} stations: #{stations.reverse}."
       end
 
-    elsif start[0].downcase == "l"
+    elsif start[0].downcase == "l" # l to l
       s_index = line_l.index(start[1])
       e_index = line_l.index(ending[1])
       if s_index < e_index
@@ -39,7 +39,7 @@ def rute(start, ending)
         puts "There are #{numbers} stations: #{stations.reverse}."
       end
 
-    elsif start[0].downcase == "6"
+    elsif start[0].downcase == "6" # 6 to 6
       s_index = line_6.index(start[1])
       e_index = line_6.index(ending[1])
       if s_index < e_index
@@ -47,15 +47,43 @@ def rute(start, ending)
         numbers = stations.count
         puts "There are #{numbers} stations: #{stations}."
       else
-        stations = line_6[e_index..s_index]
+        stations = line_6[e_index..s_index].reverse
         numbers = stations.count
-        puts "There are #{numbers} stations: #{stations.reverse}."
+        puts "There are #{numbers} stations: #{stations}."
       end
     else
       puts "There is no such line."
     end
+  else
+    if start[0].downcase == "n" # start from n
+      s_index = line_n.index(start[1])
+      u_n_index = line_n.index('union square')
+      if s_index < u_n_index
+        f_stations = line_n[s_index..u_n_index]
+      else
+        f_stations = line_n[u_n_index..s_index].reverse
+      end
+      if ending[0] == 'l' # from n to l
+        e_index = line_l.index(ending[1])
+        u_l_index = line_l.index('union square')
+        if u_l_index < e_index
+          l_stations = line_l[u_l_index..e_index]
+          stations = (f_stations + l_stations).uniq
+          numbers = stations.count
+          puts "There are #{numbers} stations: #{stations}."
+        else
+          l_stations = line_l[e_index..u_l_index].reverse
+          stations = (f_stations + l_stations).uniq
+          numbers = stations.count
+          puts "There are #{numbers} stations: #{stations}."
+        end
+      end
+    else
+      puts "Waiting"
+    end
   end
 end
+
 
 rute(['n', '8th'],['n', 'time square'])
 rute(['n', '28th'],['n', '8th'])
@@ -64,5 +92,6 @@ rute(['l', '1st'],['l', '8th'])
 rute(['6', 'grand central'],['6', '23rd'])
 rute(['6', 'astro place'],['6', '23rd'])
 rute(['4','iii'],['4','858'])
+rute(['n', '8th'],['l', '1st'])
 
 binding.pry
