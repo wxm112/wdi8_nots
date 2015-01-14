aa326fd06dcfa315407e16715af0dcd8cd6a6bc1
王雪梅 <wxm112@me.com>
2015-01-14 18:53:33 +1100

    simplified same line

--- mta.rb
+++ mta.rb
@@ -8,51 +8,28 @@ LINE_N = ['time square', '34th', '28th', '23rd','union square', '8th']
 LINE_L = ['8th', '6th', 'union square', '3rd', '1st']
 LINE_6 = ['grand central', '33rd', '28th', '23rd', 'union square', 'astro place']
 
-
+LINES = { 'n' => LINE_N, 'l' => LINE_L, '6' => LINE_6 }
 
 def route(start, ending)
   if start[0] == ending[0]
-    if start[0].downcase == "n" # n to n
-      s_index = LINE_N.index(start[1])
-      e_index = LINE_N.index(ending[1])
-      if s_index < e_index
-        stations = LINE_N[s_index..e_index]
-        numbers = stations.count
-        puts "There are #{numbers} stations: #{stations}."
-      else
-        stations = LINE_N[e_index..s_index]
-        numbers = stations.count
-        puts "There are #{numbers} stations: #{stations.reverse}."
-      end
+    line = LINES[start[0].downcase]
 
-    elsif start[0].downcase == "l" # l to l
-      s_index = LINE_L.index(start[1])
-      e_index = LINE_L.index(ending[1])
-      if s_index < e_index
-        stations = LINE_L[s_index..e_index]
-        numbers = stations.count
-        puts "There are #{numbers} stations: #{stations}."
-      else
-        stations = LINE_L[e_index..s_index]
-        numbers = stations.count
-        puts "There are #{numbers} stations: #{stations.reverse}."
-      end
-
-    elsif start[0].downcase == "6" # 6 to 6
-      s_index = LINE_6.index(start[1])
-      e_index = LINE_6.index(ending[1])
-      if s_index < e_index
-        stations = LINE_6[s_index..e_index]
-        numbers = stations.count
-        puts "There are #{numbers} stations: #{stations}."
-      else
-        stations = LINE_6[e_index..s_index].reverse
-        numbers = stations.count
-        puts "There are #{numbers} stations: #{stations}."
-      end
+    if line.nil?
+      puts "There is no such line: #{start[0]}"
     else
-      puts "There is no such line."
+      s_index = line.index(start[1])
+      e_index = line.index(ending[1])
+      if s_index < e_index
+        stations = line[s_index..e_index]
+        numbers = stations.count
+        puts "There are #{numbers} stations: #{stations}."
+      else
+        stations = line[e_index..s_index]
+        numbers = stations.count
+        puts "There are #{numbers} stations: #{stations.reverse}."
+      end
     end
+
   else
     if start[0].downcase == "n" # start from n
       s_index = LINE_N.index(start[1])
@@ -93,4 +70,3 @@ route(['6', 'astro place'],['6', '23rd'])
 route(['4','iii'],['4','858'])
 route(['n', '8th'],['l', '1st'])
 
-binding.pry
