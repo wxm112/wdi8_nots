971a64e3a12aa9d367df8968412d694a36f2aad8
王雪梅 <wxm112@me.com>
2015-01-15 21:16:43 +1100

    simplify three create functions

--- rental_lab/main.rb
+++ rental_lab/main.rb
@@ -23,6 +23,10 @@ require_relative 'building'
 #   end
 # end
 # read_choice
+tenants = []
+apartments = []
+buildings = []
+
 
 def create_tenant
   puts "Tenant Creation:"
@@ -35,12 +39,10 @@ def create_tenant
   print "Enter tenant gender: "
   gender = gets.chomp
 
-  tenant = Tenant.new name, age, gender
-  tenant
+  Tenant.new name, age, gender
 end
 
-# tenants = []
-# tenants << create_tenant
+ tenants << create_tenant
 
 def create_apartment
   puts "Apartment Creation:"
@@ -56,55 +58,37 @@ def create_apartment
   print "Enter the number of bedrooms: "
   num_bedrooms = gets.chomp
 
-  apartment = Apartment.new name, sqft, num_bathrooms, num_bedrooms
+	Apartment.new name, sqft, num_bathrooms, num_bedrooms
+end
 
-  print "Is there any tenant? (y/n): "
-  answer = gets.chomp
-  if answer == "y"
-    apartment.tenants << create_tenant
-  end
-    apartment
-  end
-
-  apartments = []
-  apartments << create_apartment
+apartments << create_apartment
 
 
-  # def create_building
-  #   puts "Building Creation:"
-  #   print "Enter the Building's address: "
-  #   address = gets.chomp
+def create_building
+  puts "Building Creation:"
+  print "Enter the Building's address: "
+  address = gets.chomp
 
-  #   print "Enter the style of the building: "
-  #   style = gets.chomp
+  print "Enter the style of the building: "
+  style = gets.chomp
 
-  #   print "If the building has doorman? (y/n): "
-  #   has_doorman = gets.chomp
+  print "If the building has doorman? (y/n): "
+  has_doorman = gets.chomp
 
-  #   print "If the building has elevator? (y/n): "
-  #   has_elevator = gets.chomp
+  print "If the building has elevator? (y/n): "
+  has_elevator = gets.chomp
 
-  #   print "Enter the number of floors: "
-  #   num_floors = gets.chomp
+  print "Enter the number of floors: "
+  num_floors = gets.chomp
 
-  #   print "Enter the apartment's number: "
-  #   apartment_numer = gets.chomp.to_i
-
-  #   print "Enter the apartment: "
-  #   apartment = gets.chomp
+  Building.new address, style, has_doorman, has_elevator, num_floors
+end
 
 
-  #   building = Building.new address, style, has_doorman, has_elevator, num_floors
-  #   building.apartments[apartment_numer] = apartment
-  #   building
-  # end
-
-  # buildings = []
-  # buildings << create_building
+buildings << create_building
 
 
 
 
-
-  require 'pry'
-  binding.pry
+require 'pry'
+binding.pry
