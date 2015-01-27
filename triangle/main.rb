989661e79722f8bd3c84c22b737144fab12ee283
王雪梅 <wxm112@me.com>
2015-01-28 10:37:03 +1100

    add triangle program

---
+++ triangle/main.rb
@@ -0,0 +1,18 @@
+def triangle(a, b, c)
+    array = [a,b,c].sort
+  if a > 0 and b >0 and c > 0
+    if array[0] + array[1] > array[2]
+      if a==b and b==c
+        :equilateral
+      elsif a==b or b==c or a==c
+        :isosceles
+      elsif a!=b and b!=c
+        :scalene
+      end
+    else
+      raise ArgumentError
+    end
+  else
+    raise ArgumentError
+  end
+end
