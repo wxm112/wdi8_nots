c66d6315e99542ba2a1440038290ebae6cefea35
王雪梅 <wxm112@me.com>
2015-01-28 10:39:10 +1100

    updated triangle

--- triangle/main.rb
+++ triangle/main.rb
@@ -1,7 +1,6 @@
 def triangle(a, b, c)
     array = [a,b,c].sort
-  if a > 0 and b >0 and c > 0
-    if array[0] + array[1] > array[2]
+  if a > 0 and b >0 and c > 0 and array[0] + array[1] > array[2]
       if a==b and b==c
         :equilateral
       elsif a==b or b==c or a==c
@@ -9,9 +8,6 @@ def triangle(a, b, c)
       elsif a!=b and b!=c
         :scalene
       end
-    else
-      raise ArgumentError
-    end
   else
     raise ArgumentError
   end
