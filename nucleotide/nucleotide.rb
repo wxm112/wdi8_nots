5882c5464b5695344ad217c2c35c3dffc4172932
王雪梅 <wxm112@me.com>
2015-02-25 09:59:20 +1100

    updated 2

--- nucleotide/nucleotide.rb
+++ nucleotide/nucleotide.rb
@@ -1,23 +1,22 @@
 class DNA
-  attr_accessor :string;
 
-def initialize (string)
-  @string = string
-  @array = ['A','T','C','G','U']
-end
-
-def count(letter)
-  if @array.include?(letter)
-    @string.count(letter) 
-  else
-    raise ArgumentError.new("That's not a nucleotie, silly!")
+  def initialize (string)
+    @string = string
+    @array = ['A','T','C','G','U']
   end
-end
 
-def nucleotide_counts
- hash = {}
- @array.each {|a| hash[a]=count(a)}
- hash
-end
+  def count(letter)
+    if @array.include?(letter)
+      @string.count(letter)
+    else
+      raise ArgumentError.new("That's not a nucleotie, silly!")
+    end
+  end
+
+  def nucleotide_counts
+    hash = {}
+    @array.each {|a| hash[a]=count(a)}
+    hash
+  end
 
 end
