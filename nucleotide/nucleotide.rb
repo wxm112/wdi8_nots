d1c9e84b3f66a77cd30daf913103af9b85dc05ef
王雪梅 <wxm112@me.com>
2015-02-25 10:24:14 +1100

    change if else to one line

--- nucleotide/nucleotide.rb
+++ nucleotide/nucleotide.rb
@@ -5,11 +5,7 @@ class DNA
   end
 
   def count(letter)
-    if "ATCGU".include?(letter)
-      @string.count(letter)
-    else
-      raise ArgumentError.new("That's not a nucleotie, silly!")
-    end
+    "ATCGU".include?(letter) ? (@string.count(letter)) : (raise ArgumentError.new("That's not a nucleotie, silly!"))
   end
 
   def nucleotide_counts
