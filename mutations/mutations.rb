9f94f722f1fb830c9ae5a9df13d65585f21dc9bd
王雪梅 <wxm112@me.com>
2015-02-26 10:34:46 +1100

    final mutation

--- mutations/mutations.rb
+++ mutations/mutations.rb
@@ -6,15 +6,10 @@ class DNA
   end
 
 
-  def hamming_distance(anther_strand)
-    array = @strand.chars
-    anther_array = anther_strand.chars
-    result = (array.each_with_index.map {|l,i| l != anther_array[i] ? 1 : nil}).compact
-    if array.length > anther_array.length
-      result.length - array.length + anther_array.length 
-    else
-      result.length
-    end
+  def hamming_distance(other_strand)
+    @strand.chars.zip(other_strand.chars).reject do |a, b|
+      a == b || b.nil?
+    end.size
   end
 
 end
