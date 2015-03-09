49cf7939c8221a15d596ab63d74a5753a3b2e22b
王雪梅 <wxm112@me.com>
2015-03-10 10:38:37 +1100

    finished sieve_test

---
+++ sieve_test/sieve.rb
@@ -0,0 +1,36 @@
+class Sieve
+
+  def initialize(number)
+    @number = number
+  end
+
+  def primes
+    list = (2..@number).to_a
+    primes = [ ]
+    while list.empty? != true do 
+      first = list[0]
+      primes << first
+      list.select! {|n| n % first != 0}
+    end
+    primes
+  end
+
+
+  
+
+  # private
+
+  # def calculate_primes
+  #   numbers = (2..limit).to_a
+  #   primes = []
+  #   begin
+  #     target = numbers.shift
+  #     primes << target  
+  #     numbers.reject! do |i|
+  #       i % target == 0
+  #     end
+  #   end until numbers.empty?
+  #   primes
+  # end
+
+end
