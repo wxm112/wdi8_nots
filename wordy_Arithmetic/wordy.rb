f3ce2aaf1585c95b3c7dbee6301ef27a3f83ee13
王雪梅 <wxm112@me.com>
2015-03-18 10:24:34 +1100

    updated answer

--- wordy_Arithmetic/wordy.rb
+++ wordy_Arithmetic/wordy.rb
@@ -1,3 +1,27 @@
+require 'pry'
+# class WordProblem
+#   def initialize(question)
+#     @question = question
+#   end
+#   def answer
+#     if matches.nil?
+#       raise ArgumentError.new "I don't understand"
+#     end 
+#     @answer = @matches[1].to_i.send( operation, @matches[3].to_i)
+#   end
+#   def matches
+#     # This is where the regexp will go
+#     @matches = @question.match( /Whis is (-?\d+) (plus|minus|multiplied by|divided by) (-?\d+)\?/ )
+#   end
+#   def operation
+#     case @matches[2]
+#     when 'plus' then :+
+#     when 'minus' then :-
+#     when 'multiplied by' then :*
+#     when 'divided by' then :/
+#     end
+#   end
+# end
 
 class WordProblem
   def initialize( question )
